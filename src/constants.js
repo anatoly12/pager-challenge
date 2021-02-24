6965ba887ae9a2d4f4a054a801dbe09e2fece096
Anatoly P <anatolypolicastroa@gmail.com>
2021-02-24 16:32:14 -0500

    Add constants

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const BASE_URL = 'http://swapi.dev/api';
+
+export const OBJECT_TYPES = ['planets', 'films', 'species', 'vehicles', 'starships'];
+
+export const PEOPLE_FIELD_NAME = {
+  planets: 'residents',
+  films: 'characters',
+  species: 'people',
+  vehicles: 'pilots',
+  starships: 'pilots',
+};
